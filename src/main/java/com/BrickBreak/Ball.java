ce6af772c72feaa4b941129a56680111f20c4406
carsonKim67 <carson.p.kim@gmail.com>
2025-12-16 21:36:05 +0000

    done

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,13 +1,11 @@
-//name
-//destription
+//Carson Kim
+//Ball class for Brick Breaker Game - represents the ball that bounces around the screen
 package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
-	//your code here!
 	
-	//don't forget you need instance variables:
-	
+    //instance variables:	
 	private int x;
 	private int y;
 	private int radius;
@@ -16,6 +14,8 @@ public class Ball {
 	
 	
 	//constructor(s):
+	//pre condition: radius is the diameter of the ball
+	//post condition: creates a ball object at position (x,y) with the given radius
 	public Ball(int x, int y, int radius){
 		this.x = x;
 		this.y = y;
@@ -30,23 +30,32 @@ public int getXpos(){
 public int getYpos(){
 	return y;
 }
+
 public void setXVelocity(int speedX){
 	this.speedX = speedX;
 }
 public int getSize(){
 	return radius;
 }
+//pre condition: g is the Graphics object to draw on
+//post condition: draws the ball on the screen
 public void draw(Graphics g){
 	g.setColor(Color.white);
 	g.fillOval(x, y, radius, radius);
 }
+//pre condition: there is a ball object
+//post condition: moves the ball according to its velocity
 public void move(){
 	x+=speedX;
 	y+=speedY;
 }
+//pre condition: paddle touches the ball
+//post condition: reverses the ball's y velocity
 public void reverseY(){
 	speedY = -speedY;
 }
+//pre condition: paddle touches the ball
+//post condition: reverses the ball's x velocity
 public void reverseX(){
 	speedX = -speedX;
 }
--- src/main/java/com/BrickBreak/Paddle.java
+++ src/main/java/com/BrickBreak/Paddle.java
@@ -1,18 +1,13 @@
+//Carson Kim
+//Paddle class for Brick Breaker Game - represents the paddle controlled by the player
 package com.BrickBreak;
 
 import java.awt.*;
 
 public class Paddle {
-
-
-	//your code here!
-
-
-	
-	//don't forget you need instance variables:
+	//instance variables:
 	private static final int MAX_SPEED = 6;
 	private static final int MIN_SPEED = -6;
-	
 	private int speed;
 	private int x;
 	private int y;
@@ -21,6 +16,8 @@ public class Paddle {
 	
 	
 	//constructor(s):
+	//pre con - given position (x,y) and dimensions width and height
+	//post con - creates a paddle object at position (x,y) with the given width
 	public Paddle(int x,int y, int width, int height){
 		this.x = x;
 		this.y = y;
@@ -29,10 +26,14 @@ public class Paddle {
 		speed = 0;
 	}
 	//methods:
+	//pre condition: g is the Graphics object to draw on
+	//post condition: draws the paddle on the screen
 	public void draw(Graphics g){
 	g.setColor(Color.white);
 	g.fillRect(x, y,width,height);
-}
+}	
+	//pre condition: there is a paddle object
+	//post condition: moves the paddle according to its velocity
 	public void move(){
 		x+= speed;
 
@@ -58,10 +59,13 @@ public class Paddle {
 	public void setVelocity(int speed){
 		this.speed = clampSpeed(speed);
 	}
+	//pre con - added speed is given
+	//post con - adds the given speed to the paddle's current speed
 	public void addVelocity(int addedSpeed){
 		speed = clampSpeed(speed + addedSpeed);
 	}
-
+	//pre con - speed to be clamped is given
+	//post con - returns the speed clamped between MAX_SPEED and MIN_SPEED
 	private int clampSpeed(int s){
 		if(s > MAX_SPEED) return MAX_SPEED;
 		if(s < MIN_SPEED) return MIN_SPEED;
